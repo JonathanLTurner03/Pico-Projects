4d298baaa3c4893f846dee71b49fff072010e20a
Jonathan Turner <turner@atlantissrv.com>
2024-04-21 15:50:23 -0400

    Implemented converter.

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ print("Beginning Check:")
 def convert_to_percentage(value):
     return (value / 65535) * 100
 
-while True:
+max = 10000
+counter = 0
+previous = 0
+
+while counter < max:
     actual = pot.read_u16()
-    print("Pos is: ", str(actual), " | Perc is: ", str(convert_to_percentage(actual)))
+    conversion = int(convert_to_percentage(actual))
+    if (conversion % 2 == 0):
+        print("Pos is: ", str(actual), " | Perc is: ", str(conversion))
     sleep(0.1)
+    counter+=1
